950c9d50dc88957c6cce05cc4c5862b53dcfe148
zhanymsoulz <islambekmadizhan@gmail.com>
2025-06-11 23:41:02 +0200

    Initial Spring Boot setup for CineVerse

---
+++ src/main/java/com/unime/CineVerse/CineVerseApplication.java
@@ -0,0 +1,13 @@
+package com.unime.CineVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unime/CineVerse/CineVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unime.CineVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
