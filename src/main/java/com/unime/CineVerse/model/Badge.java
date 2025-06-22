77dbfa59852242672381b6b32723d79cc38c3983
Hp <adkhamjon.abd@gmail.com>
2025-06-22 11:30:57 +0200

    Add Badge and UserBadge entities with relationships

---
+++ src/main/java/com/unime/CineVerse/model/Badge.java
@@ -0,0 +1,27 @@
+package com.unime.CineVerse.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String rule;
+    private String description;
+    private String url;
+}
